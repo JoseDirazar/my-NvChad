23f089c17e381cc0a1bd42fac829c69737cfbf33
JoseDirazar <jose_f_dirazar@hotmail.com>
2024-11-03 11:41:17 -0300

    update wez conf

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,25 +3,27 @@ local wezterm = require 'wezterm'
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
-local dimmer = { brightness = 0.1 }
+local dimmer = { brightness = 0.07 }
 
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'AdventureTime'
+config.color_scheme = 'Belafonte Night (Gogh)'
 --config.window_background_image = './Imágenes/term-bg/dark-abstract-ubuntu-hd-8yw4fdw80j4hjyrp.jpg'
 config.background = {
   {
-    source =  {
-      File = './Imágenes/term-bg/ubuntu-blue-bg.jpg'},
+    source = {
+      File = './Pictures/landscape.jpg' },
     width = 'Cover',
     horizontal_align = 'Center',
     vertical_align = "Middle",
-    hsb = dimmer
+    hsb = dimmer,
   },
 }
-
-
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+-- config.window_background_opacity = 0.9
+config.enable_tab_bar = false
+config.window_decorations = "NONE"
 
 -- and finally, return the configuration to wezterm
 return config
